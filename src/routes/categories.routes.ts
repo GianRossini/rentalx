331a4d9491c24f9bf1d33d4924373cbeb73d9985
GianRossini <gianrossini.developer@gmail.com>
2022-03-14 12:14:49 -0300

    uses uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuid(),
     name,
     description,
-  });
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
