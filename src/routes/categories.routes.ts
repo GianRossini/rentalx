1618dba25ef398d5e15b496214526b8e55eb54d0
GianRossini <gianrossini.developer@gmail.com>
2022-03-15 13:10:25 -0300

    creates get all categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -6,6 +6,12 @@ const categoriesRoutes = Router();
 
 const categoriesRepository = new CategoriesRepository();
 
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+});
+
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
